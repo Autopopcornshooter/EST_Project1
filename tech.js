932b6db12e7d277a5b028563ee1f9f1e067e233f
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2025-07-28 17:52:02 +0900

    Feat: 구독 영상 리스트 화면 구성 완료/ 영상 재생 화면 구성 진행

--- tech.js
+++ tech.js
@@ -31,28 +31,62 @@
 //     }
 // }
 
-function loadMainPage() {
+function load_mainPageLayout() {
     fetch("secondNavbar.html")
         .then(res => res.text())    //받아온 데이터를 텍스트로 파싱(분석) 과정
         .then(data => {             //data : text로 파싱된 데이터
             document.body.innerHTML += data;
             //문서에 데이터 삽입(target은 목표 div)
+            load_sidebar();
+            load_mainContentArea();
         })
         .catch(err => console.error("불러오기 실패:", err));
-    //실패한다면 오류 캐치
-    loadContentCard();
-}
 
-function loadSubscribePage() {
-    fetch("subscribeElement.html")
+
+    //실패한다면 오류 캐치
+}
+function load_subscribePageLayout() {
+    fetch("subscribeLayout.html")
         .then(res => res.text())
         .then(data => {
-            document.getElementById("contentArea").innerHTML += data;
+            document.body.innerHTML += data;
+            load_sidebar();
+            load_mainContentArea();
         })
         .catch(err => console.error("불러오기 실패:", err));
-        loadContentCard();
 }
-function loadContentCard() {
+function load_playScreenLayout() {
+    fetch("playScreenLayout.html")
+        .then(res => res.text())
+        .then(data => {
+            document.body.innerHTML += data;
+            load_listCard();
+            load_listCard();
+            load_listCard();
+            load_listCard();
+        })
+        .catch(err => console.error("불러오기 실패:", err));
+}
+function load_sidebar() {
+    fetch("sidebar.html")
+        .then(res => res.text())
+        .then(data => {
+            document.body.innerHTML += data;
+        })
+        .catch(err => console.error("불러오기 실패:", err));
+}
+function load_mainContentArea() {
+    fetch("indexLayout.html")
+        .then(res => res.text())
+        .then(data => {
+            document.body.innerHTML += data;
+            load_contentCard();
+        })
+        .catch(err => console.error("불러오기 실패:", err));
+}
+
+
+function load_contentCard() {
     fetch("content-card.html")
         .then(res => res.text())    //받아온 데이터를 텍스트로 파싱(분석) 과정
         .then(data => {             //data : text로 파싱된 데이터
@@ -62,8 +96,16 @@ function loadContentCard() {
         .catch(err => console.error("불러오기 실패:", err));
     //실패한다면 오류 캐치
 }
+function load_listCard() {
+    fetch("list-card.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("content-list").innerHTML += data;
+        })
+        .catch(err => console.error("불러오기 실패:", err));
+}
 
-loadMainPage();
+load_playScreenLayout();
 
 //html 파일 코드 불러오기
 //첫번째 then은 데이터 파싱, 두번째 then은 데이터 활용
